88c3c9f6fe9d669115c84694d76beb6d7e6f6ff1
OMsrisairam <rajeshwarbairy@gmail.com>
2017-11-27 05:37:46 -0500

    codecomit

---
+++ onlineshopping/src/main/java/net/rrb/onlineshopping/controller/PageController.java
@@ -0,0 +1,17 @@
+package net.rrb.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		ModelAndView modelAndView = new ModelAndView("page");
+		modelAndView.addObject("greeting", "Welcome to spring mvc");
+		return modelAndView;
+	}
+
+}
